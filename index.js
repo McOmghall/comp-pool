37abef128cf666e3750bb7cee865014face6c67a
pmontoto <pmgberm@gmail.com>
2014-12-03 16:19:16 +0100

    Added config info to startup

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express')
-  , app = express();
+  , app     = express()
+  , packInf = require('./package.json')
+  , host    = require('os').hostname()
+  , port    = process.env.PORT || 3000;
 
+app.set('host', host);
+app.set('port', port);
+app.listen(3000);
 
-
-
-
-console.log('Example app listening at http://%s:%s', app.address().host, app.address().port)
+console.log('%s listening at http://%s:%s', packInf.name, app.get('host'), app.get('port'));
+console.log('%s %s environment', packInf.name, app.get('env'));
