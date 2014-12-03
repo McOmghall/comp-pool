c76665034367bb4e00f0b8996b61bb5d3776bc66
pmontoto <pmgberm@gmail.com>
2014-12-03 17:39:19 +0100

    HATEOAS proper routing start with restify

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var express = require('express')
-  , app     = express()
-  , packInf = require('./package.json')
+var packInf = require('./package.json')
+  , restify = require('restify')
+  , server  = restify.createServer({
+      "name" : "server: " + packInf.name + " v" + packInf.version,
+      "formatters" : {
+        "application/bs" : function bsfy(request, response, body) {
+          response.charSet('utf8');
+          response.contentType = "text/plain";
+          response.send(417, "bullshit".toString('base64'));
+        }
+      },
+      "version" : packInf.version
+    })
   , host    = require('os').hostname()
-  , port    = process.env.PORT || 3000;
+  , port    = process.env.PORT || 3000
+  , env     = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = env;
 
-app.set('host', host);
-app.set('port', port);
-app.listen(3000);
 
-console.log('%s %s environment', packInf.name, app.get('env'));
+server.listen(port);
+
+console.log('[SUCCESS] %s:%s running %s %s environment', host, port, server.name, process.env.NODE_ENV);
