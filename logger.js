8696740bb3ca93efb73ef6dc9c60720526664b00
McOmghall <pmgberm@gmail.com>
2016-02-21 09:38:55 +0100

    Refactored and simplified logger creatin and config

--- logger.js
+++ logger.js
@@ -1,4 +1,5 @@
 var bunyan = require('bunyan')
+var _ = require('underscore')
 var option_defaults = {
   name: 'comp-pool',
   streams: [{
@@ -17,32 +18,19 @@ module.exports.getDefaultLogger = createLogger
 
 var logger = null
 
-function extend (destination, source) {
-  for (var k in source) {
-    if (source.hasOwnProperty(k)) {
-      destination[k] = source[k]
-    }
-  }
-  return destination
-}
-
 function createLogger (options) {
   options = options || {}
 
   if (!logger) {
-    mkdirSync('./logs')
-    logger = bunyan.createLogger(extend(option_defaults, options))
+    try {
+      require('fs').mkdirSync(path)
+    } catch (e) {
+      if (String(e.code) !== 'EEXIST') {
+        throw e
+      }
+    }
+    logger = bunyan.createLogger(_.extendOwn(option_defaults, options))
   }
 
   return logger
-}
-
-function mkdirSync (path) {
-  try {
-    require('fs').mkdirSync(path)
-  } catch (e) {
-    if (String(e.code) !== 'EEXIST') {
-      throw e
-    }
-  }
 }
